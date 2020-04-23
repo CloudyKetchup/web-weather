c7740de4a4c292e080aa945e7fd548231e964a4c
Krypton17 <maxdodon25@gmail.com>
2020-04-23 22:53:37 +0300

    added AccountPane

--- web-app/src/components/Main/MainBody.tsx
+++ web-app/src/components/Main/MainBody.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState, CSSProperties, FC } from "react";
+import React, { useState, useEffect, CSSProperties, FC } from "react";
+
+import AccountPane from "../AccountPane/AccountPane";
 
 import { ReactComponent as GithubIcon } from "../../assets/icons/github.svg";
+import { ReactComponent as AccountIcon } from "../../assets/icons/account.svg";
 
 import "./main-body.css";
 
-type AppInfoState = {
+type StyleProps = {
+	className? : string
 	style? : CSSProperties
 };
 
-const AppInfo : FC<AppInfoState> = props =>
+const AppInfo : FC<StyleProps> = props =>
 (
 	<div className="app-info" style={props.style}>
 		<div>
@@ -20,17 +24,77 @@ const AppInfo : FC<AppInfoState> = props =>
 	</div>
 );
 
+interface AccountButtonProps extends StyleProps { onClick : () => void }
+
+const AccountButton : FC<AccountButtonProps> = props =>
+(
+	<button
+		className={props.className}
+		style={props.style}
+		onClick={props.onClick}
+	>
+		<AccountIcon/>
+	</button>
+);
+
 const MainBody = () =>
 {
 	const [appInfoStyle, setAppInfoStyle] = useState({ display : "none" });
+	const [accountPane, setAccountPane] = useState(false);
+	let accountPaneTimer : NodeJS.Timeout | null;
+	
+	useEffect(() =>
+	{
+		let appInfoTimeout : NodeJS.Timeout | null = setTimeout(() =>
+		{
+			if (appInfoStyle.display !== "flex")
+			{
+				setAppInfoStyle({ display : "flex" })
 
-	setTimeout(() => setAppInfoStyle({ display : "flex" }), 800);
+				appInfoTimeout && clearTimeout(appInfoTimeout);
+				appInfoTimeout = null;
+			}
+		}, 500);
+	});
+
+	const toggleAccountPane = async () =>
+	{
+		if (!accountPaneTimer)
+		{
+			if (accountPane)
+			{
+				await accountPaneSlideBack()
+
+				accountPaneTimer = setTimeout(async () =>
+				{
+					setAccountPane(!accountPane)
+
+					accountPaneTimer && clearTimeout(accountPaneTimer);
+					accountPaneTimer = null;
+				}, 50);
+			} else setAccountPane(!accountPane);
+		}
+	};
+
+	const accountPaneSlideBack = async () =>
+	{
+		const accountPaneDiv = document.getElementsByClassName("account-pane")[0] as HTMLElement;
+
+		if (accountPaneDiv != null)
+		{
+			accountPaneDiv.style.opacity = "0";
+			accountPaneDiv.style.right = "-50px";
+		}
+	};
 
 	return (
 		<div className="main-body">
-			<div>
-				<AppInfo style={appInfoStyle}/>
-			</div>
+			<AccountButton
+				className={"account-button-active"}
+				onClick={toggleAccountPane}
+			/>
+			{accountPane && <AccountPane/>}
+			<AppInfo style={appInfoStyle}/>
 		</div>
 	);
 };
