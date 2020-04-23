82c48f893e423dcb3567069454ea5766f7809fb4
Krypton17 <maxdodon25@gmail.com>
2020-04-23 22:51:55 +0300

    Account Context implementation

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import PageBackground from "./components/PageBackground/PageBackground"
 import MainBody from "./components/Main/MainBody";
 
+import { AccountProvider } from "./context/AccountContext";
+
 import './App.css';
 
-const App = () =>
-(
-  <div className="main-container">
-    <PageBackground/>
-    <MainBody/>
-  </div>
+const App = () => (
+	<div className="main-container">
+  	<PageBackground/>
+		<AccountProvider>
+		  	<MainBody/>
+		</AccountProvider>
+	</div>
 );
 
 export default App;
